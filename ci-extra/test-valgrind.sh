19b479be5b618f68c45a72fa310f7ccea386847d
Artem Koton <kotonartem@protonmail.com>
2024-04-13 22:16:23 +0300

    Bump template repository

--- ci-extra/test-valgrind.sh
+++ ci-extra/test-valgrind.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -euo pipefail
-IFS=$' \t\n'
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BUILD_TYPE=$1
 
-valgrind --tool=memcheck --gen-suppressions=all --leak-check=full --show-leak-kinds=all --leak-resolution=med --track-origins=yes --vgdb=no --error-exitcode=1 --suppressions="${SCRIPT_DIR}/valgrind.suppressions" cmake-build-RelWithDebInfo/tests
+SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+
+valgrind --tool=memcheck \
+  --gen-suppressions=all \
+  --leak-check=full \
+  --show-leak-kinds=all \
+  --leak-resolution=med \
+  --track-origins=yes \
+  --vgdb=no \
+  --error-exitcode=1 \
+  --suppressions="${SCRIPT_DIR}/valgrind.suppressions" \
+  cmake-build-"$BUILD_TYPE"/tests
